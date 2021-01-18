8dc39d585d1646865556f4f6fde51bce11093776
Mohamed Hossain <mz.hossain212@gmail.com>
2021-01-18 17:57:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-  array = [1,2,3]
-new_array = []
-
-array.length.times do |index|
-  new_array.push(array[index] **2)
-end
-new_array
+  new_array = []
+  counter = 0
+  while counter < numbers.length()
+    new_array.push(numbers[counter]**2)
+    counter +=1
+  end
+  return new_array
 end
